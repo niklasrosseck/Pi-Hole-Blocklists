bc31154a9129b691bd9caac7dcdee665ff707a74
niklasrosseck <65615770+niklasrosseck@users.noreply.github.com>
2024-06-11 20:57:50 +0200

    Added script to check differences between files

---
+++ check.py
@@ -0,0 +1,29 @@
+def read_file_to_list(filepath):
+    with open(filepath, 'r') as file:
+        return [line.strip().lower().replace('\t', ' ') for line in file]
+
+def check_entries(file1, file2):
+    entries = read_file_to_list(file1)
+    target_entries = set(read_file_to_list(file2))
+
+    missing_entries = [entry for entry in entries if entry not in target_entries]
+
+    with open(file2, 'a') as file:
+        for entry in missing_entries:
+            file.write(f"{entry}\n")
+
+    return missing_entries
+
+def main():
+    # The file you want to check in
+    file1 = 'Blocklists/SocialMedia/instagram_test.txt'
+    # The file you want to append with missing entries
+    file2 = 'Blocklists/SocialMedia/instagram.txt'
+
+    missing_entries = check_entries(file1, file2)
+
+    for entry in missing_entries:
+        print(f"{entry}: Not present in file")
+
+if __name__ == "__main__":
+    main()   
